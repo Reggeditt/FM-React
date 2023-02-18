70f564c21978d08adcc210afe6ffc476e6d3c9fe
Reggeditt <tkodjosarso@gmail.com>
2023-02-18 09:21:10 +0000

    setup vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const App = () => {
   return React.createElement("div", { class: "App" }, [
     React.createElement("h1", {}, "Adopt Me!"),
