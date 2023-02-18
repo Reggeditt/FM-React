489d0aef839ef37fba6196dcad85cb92deb55b79
Reggeditt <tkodjosarso@gmail.com>
2023-02-18 08:58:40 +0000

    setup npm, prettier, eslint

---
+++ src/App.js
@@ -0,0 +1,32 @@
+const App = () => {
+  return React.createElement("div", { class: "App" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Skippy",
+      breed: "Sherpherd",
+    }),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Luna",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Luna",
+      breed: "Havanese",
+    }),
+  ]);
+};
+
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
