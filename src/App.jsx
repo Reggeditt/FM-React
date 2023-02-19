29698a7a98d468e72899fb0810b81eefb3c8a588
Reggeditt <tkodjosarso@gmail.com>
2023-02-19 05:36:20 +0000

    write jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import React from "react";
 import ReactDOM from "react-dom";
+import React from "react";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { class: "App" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Skippy",
-      breed: "Sherpherd",
-    }),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Luna",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Luna",
-      breed: "Havanese",
-    }),
-  ]);
-};
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
